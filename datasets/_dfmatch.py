92713eb8bdc05b990a650063e2be74b29679bc0b
alexhe <alexhe12345678@gmail.com>
2025-05-14 16:56:04 +0800

    Add DFAUST dataset

--- datasets/_dfmatch.py
+++ datasets/_dfmatch.py
@@ -4,7 +4,9 @@ import os, sys, glob, torch
 import numpy as np
 import torch
 import random
+from typing import List, Dict
 from scipy.spatial.transform import Rotation
+from tqdm import tqdm
 from torch.utils.data import Dataset
 from lib.benchmark_utils import to_o3d_pcd, to_tsfm, KDTree_corr
 from lib.utils import load_obj
@@ -12,13 +14,21 @@ HMN_intrin = np.array( [443, 256, 443, 250 ])
 cam_intrin = np.array( [443, 256, 443, 250 ])
 
 from lib.benchmark_utils import to_o3d_pcd, to_tsfm, get_correspondences
-from lib.datasets.df_utils import DfaustTrain
+from datasets.df_utils import DfaustTrain
+from datasets.gen_df_train import generate_training_labels, generate_evaluation_labels
+
+
+CACHE_DF = True  # Use true for actual training
+NUM_EVAL_FRAME = 24
+NUM_REPEAT = 10
+NUM_KNN = 4
+ROOT_DIR_DF = '/home/idarc/hgz/SuperGraph/DeformationPyramid/data/DFAUST'
 
 
 class _DFMatch(Dataset):
 
     def __init__(self, config, split, data_augmentation=True):
-        super(_4DMatch, self).__init__()
+        super(_DFMatch, self).__init__()
 
         assert split in ['train','val','test']
 
@@ -27,7 +37,12 @@ class _DFMatch(Dataset):
         else :
             d_slice = None
 
-        self.entries = self.read_entries(  config.split[split] , config.data_root, d_slice=d_slice )
+        # self.entries = self.read_entries(  config.split[split] , config.data_root, d_slice=d_slice )
+
+        if CACHE_DF:
+            self.data: List = self.cache_data(split, num_repeat=NUM_REPEAT)  # 400 * 10 = 4000
+        else:
+            self.data = DfaustTrain(split)
 
         self.base_dir = config.data_root
         self.data_augmentation = data_augmentation
@@ -41,32 +56,62 @@ class _DFMatch(Dataset):
 
         self.cache = {}
         self.cache_size = 30000
+    
+    def get_entry(self, index):
+        index_valid = int(index % len(self.data)) 
+        if CACHE_DF:
+            return self.data[index_valid]
+        else:
+            data = self.data[index_valid]
+            return generate_training_labels(data, knn=NUM_KNN)
+    
+    def cache_data(self, split, num_repeat: int = 1):
+        df_dataset = self.get_df_dataset(split)
+        df_len = len(df_dataset)
+        df_total = int(num_repeat * df_len)
+        data = []
+        for i_total in tqdm(range(df_total)):
+            i_repeat = i_total % df_len  # Useless
+            i_df = i_total // df_len
+            data = df_dataset[i_df]
+            entry: Dict = generate_training_labels(data, knn=NUM_KNN)
+            data.append(entry)
+        return data
 
+    def get_df_dataset(self, split):
+        # The notion is that for training split we do not need 24 frames for every sample
+        # However for evaluation we want the while 24 of them
+        return DfaustTrain(
+            root = ROOT_DIR_DF,
+            split = 'train' if split == 'train' else 'test',
+            max_frame = 1 if split == 'train' else NUM_EVAL_FRAME,
+        )
 
-    def read_entries (self, split, data_root, d_slice=None, shuffle= False):
-        entries = glob.glob(os.path.join(data_root, split, "*/*.npz"))
-        if shuffle:
-            random.shuffle(entries)
-        if d_slice:
-            return entries[:d_slice]
-        return entries
-
+    # def read_entries (self, split, data_root, d_slice=None, shuffle= False):
+    #     entries = glob.glob(os.path.join(data_root, split, "*/*.npz"))
+    #     if shuffle:
+    #         random.shuffle(entries)
+    #     if d_slice:
+    #         return entries[:d_slice]
+    #     return entries
 
     def __len__(self):
-        return len(self.entries )
-
+        if CACHE_DF:
+            return len(self.data)
+        else:
+            return int(len(self.data) * NUM_REPEAT)
 
     def __getitem__(self, index, debug=False):
 
+        # if index in self.cache:
+        #     entry = self.cache[index]
 
-        if index in self.cache:
-            entry = self.cache[index]
-
-        else :
-            entry = np.load(self.entries[index])
-            if len(self.cache) < self.cache_size:
-                self.cache[index] = entry
-
+        # else :
+        #     entry = np.load(self.entries[index])
+        #     if len(self.cache) < self.cache_size:
+        #         self.cache[index] = entry
+        
+        entry = self.get_entry(index=index)
 
         # get transformation
         rot = entry['rot']
@@ -81,8 +126,6 @@ class _DFMatch(Dataset):
         else:
             metric_index = None
 
-
-
         # if we get too many points, we do some downsampling
         if (src_pcd.shape[0] > self.max_points):
             idx = np.random.permutation(src_pcd.shape[0])[:self.max_points]
@@ -90,7 +133,6 @@ class _DFMatch(Dataset):
         if (tgt_pcd.shape[0] > self.max_points):
             idx = np.random.permutation(tgt_pcd.shape[0])[:self.max_points]
             tgt_pcd = tgt_pcd[idx]
-
 
         if debug:
             import mayavi.mlab as mlab
@@ -103,8 +145,6 @@ class _DFMatch(Dataset):
             mlab.points3d(src_pcd[ :, 0] , src_pcd[ :, 1], src_pcd[:,  2], scale_factor=scale_factor , color=c_red)
             mlab.points3d(tgt_pcd[ :, 0] , tgt_pcd[ :, 1], tgt_pcd[:,  2], scale_factor=scale_factor , color=c_blue)
             mlab.show()
-
-
 
         # add gaussian noise
         if self.data_augmentation:
@@ -124,7 +164,6 @@ class _DFMatch(Dataset):
             tgt_pcd += (np.random.rand(tgt_pcd.shape[0], 3) - 0.5) * self.augment_noise
             s2t_flow = src_pcd_deformed - src_pcd
 
-
         if debug:
             # wrapp_src = (np.matmul(rot, src_pcd.T)+ trans).T
             src_wrapped = (np.matmul( rot, src_pcd_deformed.T ) + trans ).T
@@ -132,16 +171,13 @@ class _DFMatch(Dataset):
             mlab.points3d(tgt_pcd[:, 0], tgt_pcd[:, 1], tgt_pcd[:, 2], scale_factor=scale_factor, color=c_blue)
             mlab.show()
 
-
         if (trans.ndim == 1):
             trans = trans[:, None]
-
 
         src_feats = np.ones_like(src_pcd[:, :1]).astype(np.float32)
         tgt_feats = np.ones_like(tgt_pcd[:, :1]).astype(np.float32)
         rot = rot.astype(np.float32)
         trans = trans.astype(np.float32)
-
 
         #R * ( Ps + flow ) + t  = Pt
         return src_pcd, tgt_pcd, src_feats, tgt_feats, correspondences, rot, trans, s2t_flow, metric_index
@@ -175,3 +211,4 @@ if __name__ == '__main__':
         except:
             # print(i, "/", len(D))
             pass
+        
